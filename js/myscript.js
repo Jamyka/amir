620030eacaa1266cc6e119d0eb47ad9e97fe4f97
Amir Magdy Monir <gmina95@gmail.com>
2021-11-05 11:19:40 +0200

    Add files via upload

--- js/myscript.js
+++ js/myscript.js
@@ -1,13 +1,22 @@
 window.onscroll = () => {
     let myBtn = document.getElementById('backtotop');
 
-    if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         myBtn.style.display = "block";
         myBtn.onclick = () => {
             document.body.scrollTop = 0;
             document.documentElement.scrollTop = 0;
-        } 
-    }else{
+        }
+    } else {
         myBtn.style.display = "none";
+    }
+}
+
+window.onload = () => {
+    let formBtn = document.getElementById('formbtn');
+    formBtn.onclick = (e) => {
+        e.defaultPrevented;
+        window.alert("Sorry, But this form is for demostrating purposes only.\n Thank You");
+
     }
 }
